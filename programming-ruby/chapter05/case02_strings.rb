cca4bcb39db4f54941a55f6ac2a52fdadb1fcc12
Kevin <huanqing19901001@163.com>
2017-11-03 11:17:25 +0800

    Add some files.

---
+++ programming-ruby/chapter05/case02_strings.rb
@@ -0,0 +1,6 @@
+# Strings:
+puts 'escape using "\\"'		# escape using "\"
+puts 'That\'s right!'			# That's right!
+
+puts "Seconds/day: #{24*60*60}"		# Seconds/day: 86400
+puts "#{'Ho! '*3}Marry Christmas!"	# Ho! Ho! Ho! Marry Christmas!
