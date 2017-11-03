cabbda81a0ca275390014bf8b4b48c292cad83ae
Kevin <huanqing19901001@163.com>
2017-11-03 22:29:27 +0800

    Add ome files.

--- programming-ruby/chapter05/case02_strings.rb
+++ programming-ruby/chapter05/case02_strings.rb
@@ -4,3 +4,25 @@ puts 'That\'s right!'			# That's right!
 
 puts "Seconds/day: #{24*60*60}"		# Seconds/day: 86400
 puts "#{'Ho! '*3}Marry Christmas!"	# Ho! Ho! Ho! Marry Christmas!
+
+
+# Code inserted to replace can be one or more than one line:
+puts "Now is #{
+  def the(a)
+    'the ' + a
+  end
+  the("time")
+} for all good coders..."		# Now is the time for all good coders...
+
+
+# %q and %Q:
+puts %q/general single-quoted string/	# general single-quoted string
+puts %Q!general double-quoted string!	# general double-quoted string
+puts %Q{Seconds/day: #{24*60*60}}	# Seconds/day: 86400
+
+
+# Use 'here document' to create string:
+string = <<END_OF_STRING
+  The body of string is the input lines up to one ending with the same text that followed the '<<'
+END_OF_STRING
+puts string
---
+++ programming-ruby/chapter05/case03_string_operations.rb
@@ -0,0 +1,64 @@
+class Song
+
+  attr_reader :title, :name, :length
+  attr_writer :title, :name, :legnth
+
+  def initialize(title, name, length)
+    @title  = title
+    @name   = name
+    @length = length
+  end
+
+  def to_s
+    "song: #@title--#@name (#@length)"
+  end
+
+end
+
+class SongList
+
+  def initialize
+    @songs = Array.new
+  end
+
+  def append(song)
+    @songs.push(song)
+    self
+  end
+
+  def delete_first
+    @songs.shift
+  end
+
+  def delete_last
+    @songs.pop
+  end
+
+  def [](index)
+    @songs[index]
+  end
+
+  # def with_title(title)
+  #   for i in (0..@songs.length)
+  #     return @songs[i] if title == @songs[i].name
+  #   end
+  #   return nil
+  # end
+
+  # Use Array#find:
+  def with_title(title)
+    @songs.find{|song| song.name == title}
+  end
+
+
+end
+
+File.open("songdata") do |song_file|
+  songs = SongList.new
+  song_file.each do |line|
+    file, length, name, title = line.chomp.split(/\s*\|\s*/)
+    name.squeeze!(" ")
+    songs.append(Song.new(title, name, length))
+  end
+  puts songs[0]
+end
