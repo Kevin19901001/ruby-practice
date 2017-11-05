9a599c62c717e884c0270d19014d0ccfad7bf914
Kevin <huanqing19901001@163.com>
2017-11-05 20:57:12 +0800

    Modified a file.

--- programming-ruby/chapter05/case03_string_operations.rb
+++ programming-ruby/chapter05/case03_string_operations.rb
@@ -19,10 +19,12 @@ class SongList
 
   def initialize
     @songs = Array.new
+    @index = WordIndex.new
   end
 
   def append(song)
     @songs.push(song)
+    @index.add_to_index(song, song.title, song.name)
     self
   end
 
@@ -50,16 +52,57 @@ class SongList
     @songs.find{|song| song.name == title}
   end
 
+  def lookup(word)
+    @index.lookup(word)
+  end
+
+  # Define an iterator:
 
 end
 
+class WordIndex
+
+  def initialize
+    @index = {}
+  end
+
+  def add_to_index(obj, *phrases)
+    phrases.each{|phrase|
+      phrase.scan(/\w[-\w']+/) do |word|
+        word.downcase!
+        @index[word] = [] if @index[word].nil?
+        @index[word].push(obj)
+      end
+    }
+  end
+
+  def lookup(word)
+    @index[word.downcase]
+  end
+
+end
+
+# Read lines from file:
 File.open("songdata") do |song_file|
   songs = SongList.new
   song_file.each do |line|
     file, length, name, title = line.chomp.split(/\s*\|\s*/)
     # Use 'String#squeeze!(s)' to delete redundant blank:
     name.squeeze!(" ")
-    songs.append(Song.new(title, name, length))
+
+    # Use 'String#split(/pattern/|s)' to split a string:
+    # mins, secs = length.split(/:/)
+
+    # Use 'String#scan" to split a string:
+    mins, secs = length.scan(/d+/)
+
+    songs.append(Song.new(title, name, mins.to_i*60 + secs.to_i))
   end
+  puts songs[2]
   puts songs[0]
+
+  puts songs.lookup("Fats")
+  puts songs.lookup("ain't")
+  puts songs.lookup("Red")
+  puts songs.lookup("WorLD")
 end
