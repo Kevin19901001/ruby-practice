5a55ee6d847196201d118357dd391f3230549b3c
Kevin <huanqing19901001@163.com>
2017-11-08 20:54:33 +0800

    Modified a file.

--- programming-ruby/chapter05/case05_reg_exp.rb
+++ programming-ruby/chapter05/case05_reg_exp.rb
@@ -24,7 +24,7 @@ def show_regexp(a, re)
   end
 end
 
-p show_regexp("Very interesting", /t/)	# Very in<<t>>ing
+p show_regexp("Very interesting", /t/)		# Very in<<t>>ing
 p show_regexp("Fats Waller", /a/)		# F<<a>>ts Waller
 p show_regexp("Fats Waller", /ll/)		# Fats Wa<<ll>er
 p show_regexp("Fats Waller", /z/)		# no match
@@ -36,31 +36,68 @@ p show_regexp("!@%&-_=", /%&/)			# !@<<%&>>-_=
 
 
 # '^' and '$':
-p show_regexp("This is\nthe time", /^the/)		# This is\n<<the>> time
-p show_regexp("This is\nthe time", /is$/)		# This <<is>>\nthe time
+p show_regexp("This is\nthe time", /^the/)	# This is\n<<the>> time
+p show_regexp("This is\nthe time", /is$/)	# This <<is>>\nthe time
 
 
 # '\A':
 p show_regexp("This is\nthe time", /\AThis/)	# <<This>> is\nthe time
-p show_regexp("This is\nthe time", /\Athe/)		# no match
+p show_regexp("This is\nthe time", /\Athe/)	# no match
 
 
 # '\b' and '\B':
-p show_regexp("This is\nthe time", /\bis/)		# This <<is>>\nthe time
+p show_regexp("This is\nthe time", /\bis/)	# This <<is>>\nthe time
 p show_regexp("This is\nthe time", /\Bis/)      # Th<<is>> is\nthe time
 
 
 # Characters:
-p show_regexp("Price $12", /[aeiou]/)			# Pr<<i>>ce $12
-p show_regexp("Price $12", /\s/)				# Price<< >>$12
-p show_regexp("Price $12", /[[:digit:]]/)		# Price $<<1>>2
-p show_regexp("Price $12", /[[:space:]]/)		# Price<< >>$12
+p show_regexp("Price $12", /[aeiou]/)		# Pr<<i>>ce $12
+p show_regexp("Price $12", /\s/)		# Price<< >>$12
+p show_regexp("Price $12", /[[:digit:]]/)	# Price $<<1>>2
+p show_regexp("Price $12", /[[:space:]]/)	# Price<< >>$12
 p show_regexp("Price $12", /[[:punct:]aeiou]/)	# Pr<<i>>ce $12
 
 
 # c1-c2
 s = "see [Design Patterns-page 123]"
-p show_regexp(s, /[A-F]/)		# see [<<D>>esign Patterns-page 123]
-p show_regexp(s, /[A-Fa-f]/)	# s<<e>>e [<<D>>esign Patterns-page 123]
-p show_regexp(s, /[0-9]/)		# see [Design Patterns-page <<1>>23]
-p show_regexp(s, /[0-9][0-9]/)	# see [Design Patterns-page <<1>><<2>>3]
+p show_regexp(s, /[A-F]/)			# see [<<D>>esign Patterns-page 123]
+p show_regexp(s, /[A-Fa-f]/)			# s<<e>>e [<<D>>esign Patterns-page 123]
+p show_regexp(s, /[0-9]/)			# see [Design Patterns-page <<1>>23]
+p show_regexp(s, /[0-9][0-9]/)			# see [Design Patterns-page <<1>><<2>>3]
+
+
+# ']' and '-'
+p show_regexp(s, /[]]/)				# see [Design Patterns-page 123<<]>>
+p show_regexp(s, /[-]/)				# see [Design Patterns<<->>page 123]
+p show_regexp(s, /[^a-z]/)			# see<< >>[Design Patterns-page 123]
+p show_regexp(s, /[^a-z\s]/)			# see <<[>>Design Patterns-page 123]
+
+
+# 'greedy' mode:
+t = "The moon is made of cheese"
+p show_regexp(t, /\w+/)				# <<The>> moon is made of cheese
+p show_regexp(t, /\s.*\s/)			# The<< moon is made of >>cheese
+p show_regexp(t, /\s.*?\s/)			# The<< moon >>is made of cheese
+p show_regexp(t, /[aeiou]{2, 99}/)		# no match, why?
+p show_regexp(t, /mo?o/)			# The <<moo>>n is made of cheese
+
+
+# '|':
+r = "red ball blue sky"
+p show_regexp(r, /d|e/)				# r<<e>>d ball blue sky
+p show_regexp(r, /al|lu/)			# red b<<al>>l blue sky
+p show_regexp(r, /red ball|angry sky/)		# <<red ball>> blue sky
+
+
+# group:
+"12:50am" =~ /(\d\d):(\d\d)(..)/
+p "Hour is #$1, minute is #$2"
+"12:50am" =~ /((\d\d):(\d\d))(..)/
+p "Time is #$1"
+p "Hour is #$2, minute is #$3"
+p "AM/PM is #$4"
+
+
+# match duplicated letter:
+p show_regexp('He said "Hello"', /(\w)\1/)	# He said "He<<ll>>o"
+p show_regexp("Mississippi", /(\w+)\1/)		# M<<ississ>>ippi
