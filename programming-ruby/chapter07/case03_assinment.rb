9b83b80c7a7db1deb2299ef5d66eb670103f1661
Kevin <huanqing19901001@163.com>
2017-11-15 19:39:03 +0800

    Add a file.

--- programming-ruby/chapter07/case03_assinment.rb
+++ programming-ruby/chapter07/case03_assinment.rb
@@ -10,3 +10,34 @@ end
 t = Test.new
 a = t.val = 2
 p a
+
+
+# Exchange values of two variables:
+x = 1
+y = 2
+p "x = #{x}"
+p "y = #{y}"
+x, y = y, x
+p "x = #{x}"
+p "y = #{y}"
+
+
+# Short cut:
+class Bowdlerize
+
+  def initialize(string)
+    @value = string.gsub(/[aeiou]/, "*")
+  end
+
+  def +(other)
+    Bowdlerize.new(self.to_s + other.to_s)
+  end
+
+  def to_s
+    @value
+  end
+
+end
+
+puts obj = Bowdlerize.new("damn")
+puts obj + "shame"
