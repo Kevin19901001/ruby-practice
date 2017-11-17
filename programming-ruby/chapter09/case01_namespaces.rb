1a8b0a77ad8a1ddc10c38a591c05d8a25544891c
Kevin <huanqing19901001@163.com>
2017-11-17 21:44:21 +0800

    Add some files.

---
+++ programming-ruby/chapter09/case01_namespaces.rb
@@ -0,0 +1,5 @@
+require 'trig'
+require 'moral'
+
+y = Trig.sin(Trig::PI/4)
+wrongdoing = Moral.sin(Moral::VERY_BAD)
---
+++ programming-ruby/chapter09/trig.rb
@@ -0,0 +1,13 @@
+module Trig
+
+  PI = 3.141592654
+
+  def Trig.sin(x)
+    "sin(x)"
+  end
+
+  def Trig.cos(x)
+    "cos(x)"
+  end
+
+end
