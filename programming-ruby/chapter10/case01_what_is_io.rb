a002104d3f9c8a554e78ba567076ab81b6ce6feb
Kevin <huanqing19901001@163.com>
2017-11-21 19:03:32 +0800

    Add a file.

---
+++ programming-ruby/chapter10/case01_what_is_io.rb
@@ -0,0 +1,11 @@
+# The first part of I/O interface:
+print "Enter your name:"
+name = gets
+puts name
+
+
+# The module 'kernel' has implemented all 'I/O' related methods: gets, open, print, printf, putc, puts, readline, readlinesand test, they make ruby programming easy.
+
+
+# The second way is using I/O object:
+# Ruby has defined a basic class 'IO' to manage input and output. Class 'File' and 'BasicSocket' are both subclass of class 'IO'.
