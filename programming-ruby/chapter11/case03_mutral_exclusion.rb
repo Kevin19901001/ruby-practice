5e7d32cf16e46978c7b8f6a4ec67574e74022b61
Kevin <huanqing19901001@163.com>
2017-11-26 17:20:30 +0800

    Modified a file.

--- programming-ruby/chapter11/case03_mutral_exclusion.rb
+++ programming-ruby/chapter11/case03_mutral_exclusion.rb
@@ -2,26 +2,55 @@
 # condition. When the condition was setted as 'true', the dispatcher would never dispatch current t
 # hreads to run. But it does not prevent creating and running new thread.
 # Monitors:
-class Counter
+# class Counter
+# 
+#   attr_reader :count
+# 
+#   def initialize
+#     @count = 0
+#   end
+
+#   def tick
+#     @count += 1
+#   end
+
+# end
+
+# c = Counter.new
+
+# t1 = Thread.new{ 100_000.times{ c.tick } }
+# t2 = Thread.new{ 100_000.times{ c.tick } }
+
+# t1.join
+# t2.join
+
+# puts c.count		# Perhaps ruby2.3 is different from ruby 1.8
+
+
+require 'monitor'
+class Counter < Monitor
 
   attr_reader :count
 
   def initialize
     @count = 0
+    super
   end
 
   def tick
-    @count += 1
+    synchronize do
+      @count += 1
+    end
   end
 
 end
 
 c = Counter.new
 
-t1 = Thread.new{ 100_000.times{ c.tick } }
-t2 = Thread.new{ 100_000.times{ c.tick } }
+t1 = Thread.new{100000.times{c.tick}}
+t2 = Thread.new{100000.times{c.tick}}
 
 t1.join
 t2.join
 
-puts c.count
+puts c.count		# => 200000
