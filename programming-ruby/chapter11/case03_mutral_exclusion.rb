cd6037f3cb7fe26a471b2e8ac33b0934bc1548ed
Kevin <huanqing19901001@163.com>
2017-11-27 17:12:13 +0800

    Modified a file.

--- programming-ruby/chapter11/case03_mutral_exclusion.rb
+++ programming-ruby/chapter11/case03_mutral_exclusion.rb
@@ -27,33 +27,71 @@
 # puts c.count		# Perhaps ruby2.3 is different from ruby 1.8
 
 
+# require 'monitor'
+
+# class Counter < Monitor
+
+#   attr_reader :count
+
+#   def initialize
+#     @count = 0
+#     super
+#   end
+
+#   def tick
+#     synchronize do
+#       @count += 1
+#     end
+#   end
+
+# end
+
+# c = Counter.new
+
+# t1 = Thread.new{100000.times{c.tick}}
+# t2 = Thread.new{100000.times{c.tick}}
+
+# t1.join
+# t2.join
+
+# puts c.count		# => 200000
+
+
+# For getting these benefits, it is unneccessary to inherit from class 'Monitor', we can include a mod
+# ule - MonitorMixin.
+# require 'monitor'
+# class Counter
+
+#   include MonitorMixin
+#   ...
+
+# end
+
+
+# If it is neccessary to synchronize a group of objects, a outside monitor is better:
 require 'monitor'
-class Counter < Monitor
+
+class Counter
 
   attr_reader :count
 
   def initialize
     @count = 0
-    super
   end
 
   def tick
-    synchronize do
-      @count += 1
-    end
+    @count += 1
   end
 
 end
 
 c = Counter.new
+lock = Monitor.new
 
-t1 = Thread.new{100000.times{c.tick}}
-t2 = Thread.new{100000.times{c.tick}}
+t1 = Thread.new{ 100000.times{ lock.synchronize{ c.tick } } }
+t2 = Thread.new{ 100000.times{ lock.synchronize{ c.tick } } }
 
 t1.join
 t2.join
 
-puts c.count		# => 200000
-
-# For getting these benefits, it is unneccessary to inherit from class 'Monitor', we can include a mod
-# ule - MonitorMixin.
+puts c.count
