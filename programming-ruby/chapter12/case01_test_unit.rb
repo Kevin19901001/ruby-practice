1c97764ad1c7f02fdda0df30f86bee0f64079814
Kevin <huanqing19901001@163.com>
2017-11-28 16:32:28 +0800

    Modified a file.

--- programming-ruby/chapter12/case01_test_unit.rb
+++ programming-ruby/chapter12/case01_test_unit.rb
@@ -34,7 +34,7 @@ require 'test/unit'
 class TestRoman < Test::Unit::TestCase
 
   def test_simple
-    #assert_equal("i",  Roman.new(1).to_s)
+    assert_equal("i",  Roman.new(1).to_s)
     assert_equal("ix", Roman.new(9).to_s)
   end
 
@@ -45,9 +45,12 @@ end
 # Started
 # .
 
-# Finished in 0.000527749 seconds.
+# Finished in 0.001966256 seconds.
 # -----------------------------------------------------------------------------------------------------
-# 1 tests, 1 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+# 1 tests, 2 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
 # 100% passed
 # -----------------------------------------------------------------------------------------------------
-# 1894.84 tests/s, 1894.84 assertions/s
+# 508.58 tests/s, 1017.16 assertions/s
+
+
+
