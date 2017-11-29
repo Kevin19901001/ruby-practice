7d044e4d02491140242fd556ad3bcb2778c702d8
Kevin <huanqing19901001@163.com>
2017-11-29 17:47:12 +0800

    Add case04.

--- programming-ruby/chapter14/case04_find_modules.rb
+++ programming-ruby/chapter14/case04_find_modules.rb
@@ -1,1 +1,12 @@
 # Where ruby finds its modules.
+# ruby -e "puts $:"
+
+# /usr/local/lib/site_ruby/2.3.0
+# /usr/local/lib/x86_64-linux-gnu/site_ruby
+# /usr/local/lib/site_ruby
+# /usr/lib/ruby/vendor_ruby/2.3.0
+# /usr/lib/x86_64-linux-gnu/ruby/vendor_ruby/2.3.0
+# /usr/lib/ruby/vendor_ruby
+# /usr/lib/ruby/2.3.0
+# /usr/lib/x86_64-linux-gnu/ruby/2.3.0
+
