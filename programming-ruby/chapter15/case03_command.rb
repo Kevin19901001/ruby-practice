d817fea8bf07fa90008be2495c8780d3c4aa5409
Kevin <huanqing19901001@163.com>
2017-12-04 19:00:49 +0800

    Modified a file.

--- programming-ruby/chapter15/case03_command.rb
+++ programming-ruby/chapter15/case03_command.rb
@@ -1,5 +1,25 @@
-# Exit from irb session oe subsession:
+# Exit from irb session or subsession:
 # exit, quit, irb_exit, irb_quit
 
 # Change configuration by command 'conf':
 # conf.xxx
+
+
+# Start an irb sub session. If object was supplied, it would be managed as self:
+# irb(object)
+
+
+# List irb sub sessions:
+# jobs, irb_jobs
+
+
+# Change to a specified irb sub session:
+# fg n, irb_fg n
+
+
+# Kill a irb sub session:
+# kill n, irb_kill n
+
+
+# Configuring the prompt:
+
