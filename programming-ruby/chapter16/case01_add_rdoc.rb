4c92bd4355800954986da7bad8da3eda5e0ff2dc
Kevin <huanqing19901001@163.com>
2017-12-07 15:23:43 +0800

    Add some files.

--- programming-ruby/chapter16/case01_add_rdoc.rb
+++ programming-ruby/chapter16/case01_add_rdoc.rb
@@ -1,1 +1,20 @@
+# ...
 # Adding rdoc to ruby code:
+# Adding ...
+# Adding ...
+class Shape
+
+  attr_reader :circle
+  attr_writer :circle
+
+  # Initialize...
+  def initialize(circle)
+    @circle = circle
+  end
+
+  # Drawing...
+  def draw
+    puts "Start to end..."
+  end
+
+end
