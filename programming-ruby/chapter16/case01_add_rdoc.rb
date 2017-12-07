9c328adde0ab2caba69af75b0b2d2d75dd6aa38d
Kevin <huanqing19901001@163.com>
2017-12-07 15:33:47 +0800

    Some changes.

--- programming-ruby/chapter16/case01_add_rdoc.rb
+++ programming-ruby/chapter16/case01_add_rdoc.rb
@@ -1,13 +1,13 @@
-# ...
-# Adding rdoc to ruby code:
-# Adding ...
-# Adding ...
+# Adding rdoc to ruby code.
+=begin rdoc
+  First example of rdoc: class Shape.
+=end
 class Shape
 
   attr_reader :circle
   attr_writer :circle
 
-  # Initialize...
+  # Initializing...
   def initialize(circle)
     @circle = circle
   end
