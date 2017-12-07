a88a8e42e90a4a81e1ca4fd8bcd9639baadd053e
Kevin <huanqing19901001@163.com>
2017-12-07 19:58:51 +0800

    Some changes.

--- programming-ruby/chapter16/case01_add_rdoc.rb
+++ programming-ruby/chapter16/case01_add_rdoc.rb
@@ -17,4 +17,27 @@ class Shape
     puts "Start to end..."
   end
 
+  # Extract the age and calculate the date of birth.
+  #--
+  # FIXME: fails if the birthday falls on Febrary 29th or
+  # if the person was born before epoch and the installed
+  # Ruby doesn't support negative time_t
+  #++
+  # The dob is returned as a time object.
+  #--
+  # But should probabaly change to use Date.
+  def dob(person)
+
+  end
+
+  # Lists are typed as indented paragraphs with
+  # * a * or-(for bullet lists),
+  # * a digit followed by a period for
+  # numbered lists,
+  # an upper or lower case letter followed
+  # by a period for alpha lists.
+  def list
+
+  end
+
 end
