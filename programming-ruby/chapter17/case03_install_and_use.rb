ca839b5d1fe402e19acaaac715ef1cf25cb11e53
Kevin <huanqing19901001@163.com>
2017-12-12 20:51:18 +0800

    Some changes.

--- programming-ruby/chapter17/case03_install_and_use.rb
+++ programming-ruby/chapter17/case03_install_and_use.rb
@@ -1,1 +1,20 @@
 # Installing and using ruby gems.
+# gem query -rn Blue
+# Command 'query' used '-n' to find any gem file matches regexp /Blue/ from the central gem repository.# Install the latest version:
+# gem install -r BlueCloth
+
+# Generating API documentation:
+# gem install -r BlueCloth --rdoc
+
+# Start gem_server:
+# gem server
+
+# require 'rubygems'
+require 'bluecloth'
+
+doc = BlueCloth::new <<MARKUP
+  This is some sample [text][1]. Just learning to use [BlueCloth][1].
+  Just a simple test.
+  [1]: http://ruby-lang.org
+MARKUP
+puts doc.to_html
