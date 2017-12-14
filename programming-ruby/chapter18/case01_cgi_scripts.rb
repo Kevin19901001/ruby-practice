1a7e7112540d25307ce6432f9de1763bb959cebf
Kevin <huanqing19901001@163.com>
2017-12-14 16:53:43 +0800

    Some changes.

--- programming-ruby/chapter18/case01_cgi_scripts.rb
+++ programming-ruby/chapter18/case01_cgi_scripts.rb
@@ -1,1 +1,14 @@
 # Writing cgi scripts:
+# Quoting:
+require 'cgi'
+puts CGI.escape("Nicholas Payton/Trumpet & Flugel Horn")
+
+puts CGI.escapeHTML("a < 100 && b > 200")
+
+puts CGI.escapeElement('<hr><a href="/mp3">Click Here</a><br>', 'A')
+
+puts CGI.unescapeHTML("a &lt; 100 &amp;&amp; b &gt; 200")
+
+
+# Query parameters:
+
