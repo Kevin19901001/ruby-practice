2ff4c3db966abcf896bb9c0ee329e52cfd274cef
Kevin <huanqing19901001@163.com>
2017-12-17 19:47:51 +0800

    Some changes.

--- well-grounded-rubyist/chapter01/practice.rb
+++ well-grounded-rubyist/chapter01/practice.rb
@@ -1,4 +1,4 @@
-# Reading temperature vavle from a file, then write the tansformation result into another file:
+# Reading temperature value from a file, then write the tansformation result into another file:
 celsius = File.read("celsius.dat").to_i
 fahrenheit = (celsius * 9 / 5) + 32
 fh_out = File.new("fahrenheit.dat", "w")
