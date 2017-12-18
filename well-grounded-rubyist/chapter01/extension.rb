b1236fda6fccf682419ff3e8069f3aba67f280a8
Kevin <huanqing19901001@163.com>
2017-12-18 20:06:06 +0800

    Add some files.

---
+++ well-grounded-rubyist/chapter01/extension.rb
@@ -0,0 +1,1 @@
+# The hinge of using extension and lib is methos 'require', the relative method is 'load'.
---
+++ well-grounded-rubyist/chapter01/loaddemo.rb
@@ -0,0 +1,4 @@
+# load demo:
+puts "This is the first program file."
+load "loadee.rb"
+puts "And back again to the first file."
