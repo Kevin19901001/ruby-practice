846286b8d23f4bbbb98a711707a5940029a6bb19
Kevin <huanqing19901001@163.com>
2017-12-21 20:28:21 +0800

    Add a file.

---
+++ well-grounded-rubyist/chapter01/gem_installation.rb
@@ -0,0 +1,18 @@
+# Intalling gem from 'rubygem.org':
+# egg:
+# gem install prawn
+
+
+# Installing gem from local:
+# egg:
+# gem install /home/me/mygems/ruport-1.4.0.gem
+
+
+# Uninstalling a gem:
+# egg:
+# gem uninstall prawn
+
+
+# Loading and using gem:
+# egg:
+# require "hoe"
