0109277cb4916055319853d8b8c5cd1e989a2be4
Kevin <huanqing19901001@163.com>
2017-12-29 20:27:38 +0800

    sOME CHANGES.

--- metaprogramming-ruby/chapter02/alphanumeric.rb
+++ metaprogramming-ruby/chapter02/alphanumeric.rb
@@ -36,3 +36,35 @@ class StringExtensionTest < Test::Unit::TestCase
   end
 
 end
+
+
+# Inside Class Definitions:
+3.times do
+  class C
+    puts "Hello"
+  end
+end
+
+
+# If it is meaning that there are three classes have been defined in the above codes? The answer is no.
+# Just try this:
+class D
+  def x
+    'x'
+  end
+end
+
+class D
+  def y
+    'y'
+  end
+end
+
+obj = D.new
+puts obj.x
+puts obj.y
+
+# When class 'D' is mentioned first time, there is no class 'D', then Ruby defined class 'D' and method 
+# 'x'.When class 'D' was mentioned second time, class 'D' exists, Ruby just open class 'D' and defined m
+# ethod 'y' for class 'D'. The 'class' keyword just bring you to the class context and let you define me
+# thod for it.
