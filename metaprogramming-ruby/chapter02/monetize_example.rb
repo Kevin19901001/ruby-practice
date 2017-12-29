89896dd7dd78e0d930a57be28d5a4338fb177d5b
Kevin <huanqing19901001@163.com>
2017-12-29 22:06:57 +0800

    sOME CHANGES.

--- metaprogramming-ruby/chapter02/monetize_example.rb
+++ metaprogramming-ruby/chapter02/monetize_example.rb
@@ -1,4 +1,7 @@
 require 'monetize'
 
-bargain_price = Monetize.from_numeric(99, "USD")
-puts bargain_price.format
+# bargain_price = Monetize.from_numeric(99, "USD")
+# puts bargain_price.format
+
+standard_price = 100.to_money("USD")
+p standard_price.format
