ce90e07162993f815f1172d2c1a3caa1e7168e1b
Kevin <huanqing19901001@163.com>
2018-01-12 22:41:53 +0800

    Some changes.

--- metaprogramming-ruby/chapter03/duplicated.rb
+++ metaprogramming-ruby/chapter03/duplicated.rb
@@ -5,4 +5,10 @@ class Computer
     @data_source = data_source
   end
 
+  def mouse
+    info = @data_source.get_mouse_info(@id)
+    price = @data_source.get_mouse_price(@id)
+    result = "Mouse: #{info} ($#{price})"
+  end
+
 end
