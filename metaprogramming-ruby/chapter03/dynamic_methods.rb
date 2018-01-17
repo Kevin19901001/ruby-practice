f000fc2b1d2cb398bd1bc8a6108afe3075dac63b
Kevin <huanqing19901001@163.com>
2018-01-17 22:32:08 +0800

    Some changes.

--- metaprogramming-ruby/chapter03/dynamic_methods.rb
+++ metaprogramming-ruby/chapter03/dynamic_methods.rb
@@ -5,12 +5,18 @@ class Computer
     @data_source = data_source
   end
 
-  def define_component(name)
-    info = @data_source.send "get_#{name}_info", @id
-    price = @data_source.send "get_#{name}_price", @id
-    result = "#{name.capitalize}: #{info} ($#{price})"
-    return "* #{result}" if price > 100
-    result
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price > 100
+      result
+    end
   end
+
+  define_component :mouse
+  define_component :cpu
+  define_component :keyboard
 
 end
