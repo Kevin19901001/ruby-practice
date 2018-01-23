c3d324ceec09eb2c98bd1263e82eb573825c30ce
Kevin <huanqing19901001@163.com>
2018-01-23 22:11:40 +0800

    Some changes.

--- metaprogramming-ruby/chapter03/method_missing.rb
+++ metaprogramming-ruby/chapter03/method_missing.rb
@@ -6,4 +6,19 @@ nick = Lawyer.new
 # nick.talk_simple
 
 # method_missing function:
-nick.send :method_missing, :my_method 
+# nick.send :method_missing, :my_method 
+
+
+class  Computer
+
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name)
+    super if @data_source.respond_to?("get_#{name}_info")
+    info = 
+  end
+
+end
