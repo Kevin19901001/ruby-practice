6dd15a2447e36500a9d07c069f3fe051c2275883
Kevin <huanqing19901001@163.com>
2018-01-31 21:07:51 +0800

    Add a file.

---
+++ metaprogramming-ruby/chapter03/blank_slate.rb
@@ -0,0 +1,17 @@
+class Computer < BasicObject
+
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name, *args)
+    super if !@data_source.response_to?("get_#{name}_info")
+    info = @data_source.send "get_#{name}_info", @id
+    price = @data_source.send "get_#{name}_price", @id
+    result = "#{name.capitalize}: #{info} ($#{price})"
+    return "* #{result}" if price > 100
+    result
+  end
+
+end
